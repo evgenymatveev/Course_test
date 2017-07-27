73ef07fb06fd08e199b998a007d4aac8100eb86c
evgenymatveev <evgeni2007@yandex.ru>
2017-07-27 15:46:16 +0300

    Task_001

--- src/main/java/ru/parsentev/task_001/Calculator.java
+++ src/main/java/ru/parsentev/task_001/Calculator.java
@@ -1,7 +1,6 @@
 package ru.parsentev.task_001;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -11,28 +10,37 @@ import static org.slf4j.LoggerFactory.getLogger;
  */
 public class Calculator {
     private static final Logger log = getLogger(Calculator.class);
+    private double result;
 
     public double getResult() {
-        throw new UnsupportedOperationException();
+        return this.result = result;
     }
 
     public void add(double first, double second) {
-        throw new UnsupportedOperationException();
+        result = first + second;
     }
 
     public void substract(double first, double second) {
-        throw new UnsupportedOperationException();
+        result = first - second;
     }
 
     public void multiple(double first, double second) {
-        throw new UnsupportedOperationException();
+        result = first * second;
     }
 
     public void div(double first, double second) {
-        throw new UnsupportedOperationException();
+        if(second == 0d) {
+            throw new IllegalStateException("На ноль делить нельзя!");
+        } else {
+            result = first / second;
+        }
     }
 
     public void expand(double first, double second) {
-        throw new UnsupportedOperationException();
+        double tmp = first;
+        for (int i = 0; i < second - 1; i++) {
+            tmp = tmp * first;
+        }
+        result = tmp;
     }
 }
