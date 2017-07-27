37448b2f8826a072dbaf7b1368ae2f65acd5ef6a
evgenymatveev <evgeni2007@yandex.ru>
2017-07-27 14:49:25 +0300

    Task_007

--- src/main/java/ru/parsentev/task_007/Expression.java
+++ src/main/java/ru/parsentev/task_007/Expression.java
@@ -1,7 +1,7 @@
 package ru.parsentev.task_007;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import ru.parsentev.task_001.Calculator;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -13,14 +13,51 @@ import static org.slf4j.LoggerFactory.getLogger;
  */
 public class Expression {
     private static final Logger log = getLogger(Expression.class);
+    private Calculator calculator;
 
     private final String expr;
 
     public Expression(final String expr) {
         this.expr = expr;
+        this.calculator = new Calculator();
     }
 
     public double calc() {
-        throw new UnsupportedOperationException();
+        String a = "";
+        String b = "";
+        String operator = "";
+        char[] str = expr.toCharArray();
+        for (int i = 0; i < str.length; i++) {
+            if(Character.isDigit(str[i])) {
+                a = a + str[i];
+            } else {
+                operator = operator + str[i];
+                if(Character.isDigit(str[i + 1])) {
+                    b = a;
+                    a = "";
+                }
+            }
+            if(!Character.isDigit(str[0]) || !Character.isDigit(str[str.length - 1])) {
+                throw new IllegalStateException("Не корректно введены данные!");
+            }
+        }
+        double a1 = Double.parseDouble(a);
+        double b1 = Double.parseDouble(b);
+        if(operator.equals("+")) {
+            calculator.add(b1, a1);
+        }
+        if(operator.equals("-")) {
+            calculator.substract(b1, a1);
+        }
+        if(operator.equals("*")) {
+            calculator.multiple(b1, a1);
+        }
+        if(operator.equals("/")) {
+            calculator.div(b1, a1);
+        }
+        if(operator.equals("exp")) {
+            calculator.expand(b1, a1);
+        }
+        return calculator.getResult();
     }
 }
