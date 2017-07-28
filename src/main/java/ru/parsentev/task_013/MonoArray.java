0602fd124a5d5f74ca3cb00a45eb94dadd0561da
evgenymatveev <evgeni2007@yandex.ru>
2017-07-28 17:55:42 +0300

    Task_013

--- src/main/java/ru/parsentev/task_013/MonoArray.java
+++ src/main/java/ru/parsentev/task_013/MonoArray.java
@@ -1,7 +1,6 @@
 package ru.parsentev.task_013;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -21,6 +20,12 @@ public class MonoArray {
     }
 
     public boolean exists() {
-        throw new UnsupportedOperationException();
+        for (int i = 1; i < values.length; i++) {
+            int tmp = values[0];
+            if (values[i] != tmp) {
+                return false;
+            }
+        }
+        return true;
     }
 }
